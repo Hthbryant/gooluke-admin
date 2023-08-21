8774abe9829b050c6539521aaf71aefabc215f7c
huang <1635618490@qq.com>
2023-08-21 20:42:19 +0800

    deferredResult 延迟返回测试

--- src/main/java/com/gooluke/web/controller/TestController.java
+++ src/main/java/com/gooluke/web/controller/TestController.java
@@ -4,6 +4,7 @@ import com.gooluke.common.utils.CoreHttpUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 /**
  * @author 咕噜科
@@ -22,5 +23,22 @@ public class TestController {
     @RequestMapping("/http")
     public String testHttp() {
         return "hello,this is admin!";
+    }
+
+    /**
+     * deferredResult 延迟返回测试
+     */
+    @RequestMapping("/deferredResult")
+    public DeferredResult<String> testDeferredResult() {
+        DeferredResult<String> deferredResult = new DeferredResult<>(5000L,"Hello World");
+        new Thread(() -> {
+            try {
+                Thread.sleep(7000L);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            deferredResult.setResult("okk");
+        }).start();
+        return deferredResult;
     }
 }
